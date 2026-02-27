45bd0864208ab4978ec2887ad8ca6edd2546fe79
Nick Bourgeois <77581387+Nick676191@users.noreply.github.com>
2026-02-27 17:32:57 -0600

    Correct the link within the python file

--- grab_csvs.py
+++ grab_csvs.py
@@ -13,14 +13,14 @@ password = os.getenv("PASSWORD")
 
 driver = webdriver.Chrome()
 
-driver.get("https://global.americanexpress.com/overview")
+driver.get("https://www.americanexpress.com/en-us/account/login")
 
 # logging into the form, making sure to wait until everything loads before proceeding
 WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//input[@name='eliloUserID']"))).send_keys(username)
 # find password input box and put it in
 driver.find_element(By.XPATH, "//input[@name='eliloPassword']").send_keys(password)
 # click the log in button
-driver.find_element(By.ID, "loginSubmit").click()
+driver.find_element(By.ID, "loginSubmit").submit()
 
 # code to keep the page open until I close it
 input("Press ENTER to exit\\n")
