75446e9e78da8536ce9dc794346402a1af5efb38
Nick Bourgeois <77581387+Nick676191@users.noreply.github.com>
2026-03-03 23:49:27 -0600

    First attempt at using playwright in python to grab the gold csv

--- grab_csvs.py
+++ grab_csvs.py
@@ -31,14 +31,26 @@ import requests
 from bs4 import BeautifulSoup as bs
 from dotenv import load_dotenv
 import os
+import datetime
 from pprint import pprint
 from playwright.sync_api import sync_playwright
+from datetime import timedelta
+import re
 
 # getting the username and password for the account
 load_dotenv()
 
 username = os.getenv("USERNAME")
 password = os.getenv("PASSWORD")
+
+# finding the current month of the year
+current_date = datetime.datetime.now()
+month_name = current_date.strftime("%B")
+day_num = current_date.day
+# finding the previous month as well
+first_day_month = current_date.replace(day=1)
+last_day_prev_month = first_day_month - timedelta(days=1)
+prev_month = last_day_prev_month.strftime("%B")
 
 def main():
     # loading up the page using the URL for the page
@@ -48,7 +60,6 @@ def main():
         browser = p.chromium.launch(channel="chrome", 
                                     headless=False,
                                     args=["--disable-blink-features=AutomationControlled"])
-        # page = browser.new_page()
         context = browser.new_context(
         user_agent="Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
         )
@@ -58,11 +69,35 @@ def main():
 
         # login to account
         page.wait_for_selector("#eliloUserID", timeout=10000)
-        page.type("#eliloUserID", username, delay=125)
-        page.type("#eliloPassword", password, delay=150)
+        page.type("#eliloUserID", username, delay=150)
+        page.type("#eliloPassword", password, delay=145)
         page.click("#loginSubmit")
 
+        # getting to the correct page to download the csvs
         page.wait_for_selector("#hamburgerMenuOpener", timeout=15000)
+        page.get_by_test_id("simple_switcher_combobox").click()
+        # clicking the element from the drop down that designates the gold card `*=` designates that only part of that word needs to be in the label
+        gold_element = page.locator('div[aria-label*="Gold Card"]')
+        gold_element.click()
+        # working within the gold card
+        page.wait_for_selector("#hamburgerMenuOpener", timeout=20000)
+        page.get_by_role("link", name="View All Recent Transactions").click()
+        page.wait_for_timeout(5000)
+        page.get_by_test_id("myca-feed-DateRangePicker-Header").click()
+        page.get_by_test_id("dateRangePicker.customDateBadge").click()
+        page.get_by_label("Open calendar").click()
+        # logic to pick custom dates for the csv
+        if day_num < 15:
+            page.get_by_label("Previous month").click()
+            prev_month_date_string = prev_month + " " + "15," + " " + str(current_date.year)
+            # page.locator(f"button[aria-label*={prev_month_date_string}]").click()
+            page.get_by_role("button", name=re.compile(prev_month_date_string, re.IGNORECASE)).click()
+            page.get_by_label("Next month").click()
+            current_date_string = month_name + " " + str(day_num) + "," + " " + str(current_date.year)
+            # page.locator(f"button[aria-label*={current_date_string}]").click()
+            page.get_by_role("button", re.compile(current_date_string, re.IGNORECASE)).click()
+            page.get_by_label("Done").click()
+            page.get_by_label("Download").click()
 
         input("Press ENTER to quit\\n")
         page.close()
