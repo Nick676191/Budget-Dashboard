166fe88ec074de77b0f856ef37bcbb6eeb3cb8f3
Nick Bourgeois <77581387+Nick676191@users.noreply.github.com>
2026-03-07 01:17:55 -0600

    Clean up the code into functions that make it easier to follow. Start to implement functions with other csv that needs to be downloaded

--- grab_csvs.py
+++ grab_csvs.py
@@ -29,79 +29,110 @@ first_day_month = current_date.replace(day=1)
 last_day_prev_month = first_day_month - timedelta(days=1)
 prev_month = last_day_prev_month.strftime("%B")
 
+# defining functions for main function
+def page_setup(p):
+    browser = p.chromium.launch(channel="chrome",
+                                headless=False,
+                                args=["--disable-blink-features=AutomationControlled"])
+    ua = UserAgent()
+    user_agent = ua.random
+    context = browser.new_context(
+    user_agent=user_agent,
+    accept_downloads=True
+    )
+    page = context.new_page()
+
+    return page
+
+def login(page):
+    page.wait_for_selector("#eliloUserID", timeout=10000)
+    page.locator("#eliloUserID").press_sequentially(username, delay=random.uniform(55, 160))
+    time.sleep(random.uniform(1, 3))
+    page.locator("#eliloPassword").press_sequentially(password, delay=random.uniform(55, 160))
+    time.sleep(random.uniform(1, 3))
+    page.click("#loginSubmit")
+
+    return page
+
+def get_to_calendar(page, card_string):
+    page.wait_for_selector("#hamburgerMenuOpener", timeout=15000)
+    page.get_by_test_id("simple_switcher_combobox").click()
+    # clicking the element from the drop down that designates the gold card `*=` designates that only part of that word needs to be in the label
+    element = page.locator(f'div[aria-label*="{card_string}"]')
+    element.click()
+    # working within the gold card
+    page.wait_for_selector("#hamburgerMenuOpener", timeout=20000)
+    time.sleep(random.uniform(2.5, 5))
+    page.get_by_role("link", name="View All Recent Transactions").click()
+    # opening the calendar
+    page.get_by_test_id("myca-feed-DateRangePicker-Header").click()
+    page.get_by_test_id("dateRangePicker.customDateBadge").click()
+    page.get_by_label("Open calendar").click()
+
+    return page
+
+def pre_fifteen_download(page):
+    page.get_by_label("Previous month").click()
+    prev_month_date_string = prev_month + " " + "15," + " " + str(current_date.year)
+    time.sleep(random.uniform(1, 3))
+    page.get_by_role("button", name=re.compile(prev_month_date_string, re.IGNORECASE)).click()
+    page.get_by_label("Next month").click()
+    current_date_string = month_name + " " + str(day_num) + "," + " " + str(current_date.year)
+    page.get_by_role("button", name=re.compile(current_date_string, re.IGNORECASE)).click()
+    time.sleep(random.uniform(2, 4))
+    page.get_by_label("Done").click()
+    page.get_by_label("Download").click()
+    time.sleep(random.uniform(1, 3))
+    # clicking into the page that comes up to finalize the download
+    page.click("label[for='myca-activity-download-body-selection-options-csv']")
+    with page.expect_download() as download_information:
+        page.click("span:text-is('Download')")
+
+    download = download_information.value
+    saved_path = os.path.expanduser("~/Downloads/" + download.suggested_filename)
+    download.save_as(saved_path)
+
+    return page
+
+def shifter(card_string):
+    name = str(current_date.day) + month_name + str(current_date.year) + f"{card_string}Expenses.csv"
+    shutil.move("/Users/nickbourgeois/Downloads/activity.csv", f"/Users/nickbourgeois/Documents/finance/expense-docs/Amex {card_string}/2026")
+    os.rename(f"/Users/nickbourgeois/Documents/finance/expense-docs/Amex {card_string}/2026/activity.csv", f"/Users/nickbourgeois/Documents/finance/expense-docs/Amex {card_string}/2026/{name}")
+
+
 def main():
     # loading up the page using the URL for the page
     login_url = "https://www.americanexpress.com/en-us/account/login"
 
     with sync_playwright() as p:
-        browser = p.chromium.launch(channel="chrome",
-                                    headless=False,
-                                    args=["--disable-blink-features=AutomationControlled"])
-        ua = UserAgent()
-        user_agent = ua.random
-        context = browser.new_context(
-        user_agent=user_agent,
-        accept_downloads=True
-        )
-        page = context.new_page()
-
+        page = page_setup(p)
         page.goto(login_url)
 
         # login to account
-        page.wait_for_selector("#eliloUserID", timeout=10000)
-        # page.type("#eliloUserID", username, delay=150)
-        page.locator("#eliloUserID").press_sequentially(username, delay=random.uniform(55, 160))
-        time.sleep(random.uniform(1, 3))
-        # page.type("#eliloPassword", password, delay=145)
-        page.locator("#eliloPassword").press_sequentially(password, delay=random.uniform(55, 160))
-        time.sleep(random.uniform(1, 3))
-        page.click("#loginSubmit")
+        page = login(page)
 
         # getting to the correct page to download the csvs
-        page.wait_for_selector("#hamburgerMenuOpener", timeout=15000)
-        page.get_by_test_id("simple_switcher_combobox").click()
-        # clicking the element from the drop down that designates the gold card `*=` designates that only part of that word needs to be in the label
-        gold_element = page.locator('div[aria-label*="Gold Card"]')
-        gold_element.click()
-        # working within the gold card
-        page.wait_for_selector("#hamburgerMenuOpener", timeout=20000)
-        time.sleep(random.uniform(2.5, 5))
-        page.get_by_role("link", name="View All Recent Transactions").click()
-        # opening the calendar
-        page.get_by_test_id("myca-feed-DateRangePicker-Header").click()
-        page.get_by_test_id("dateRangePicker.customDateBadge").click()
-        page.get_by_label("Open calendar").click()
+        page = get_to_calendar(page, "Gold Card")
+
         # logic to pick custom dates for the csv
         if day_num < 15:
-            page.get_by_label("Previous month").click()
-            prev_month_date_string = prev_month + " " + "15," + " " + str(current_date.year)
-            time.sleep(random.uniform(1, 3))
-            page.get_by_role("button", name=re.compile(prev_month_date_string, re.IGNORECASE)).click()
-            page.get_by_label("Next month").click()
-            current_date_string = month_name + " " + str(day_num) + "," + " " + str(current_date.year)
-            page.get_by_role("button", name=re.compile(current_date_string, re.IGNORECASE)).click()
-            time.sleep(random.uniform(2, 4))
-            page.get_by_label("Done").click()
-            page.get_by_label("Download").click()
-            time.sleep(random.uniform(1, 3))
-            # clicking into the page that comes up to finalize the download
-            page.click("label[for='myca-activity-download-body-selection-options-csv']")
-            with page.expect_download() as download_information:
-                page.click("span:text-is('Download')")
-
-            download = download_information.value
-            saved_path = os.path.expanduser("~/Downloads/" + download.suggested_filename)
-            download.save_as(saved_path)
-
-            print(f"Saved to {saved_path}")
-            
+            page = pre_fifteen_download(page)
         else:
             print("Getting around to it")
         
         # moving the first gold file to its correct location and renaming it
-        gold_name = month_name + str(current_date.year) + "GoldExpenses.csv"
-        shutil.move("/Users/nickbourgeois/Downloads/activity.csv", "/Users/nickbourgeois/Documents/finance/expense-docs/Amex Gold/2026")
-        os.rename("/Users/nickbourgeois/Documents/finance/expense-docs/Amex Gold/2026/activity.csv", f"/Users/nickbourgeois/Documents/finance/expense-docs/Amex Gold/2026/{gold_name}")
+        shifter("Gold")
+
+        # getting the platinum card next
+        page = get_to_calendar(page, "Platinum")
+
+        if day_num < 15:
+            page = pre_fifteen_download(page)
+        else:
+            print("Getting to it")
+        
+        # moving the first gold file to its correct location and renaming it
+        shifter("Platinum")
 
         input("Press ENTER to quit\\n")
         page.close()
